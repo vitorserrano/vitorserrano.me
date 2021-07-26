fc64117393700168520a13d68cd27992ee3477a8
vitorserrano <vitorcirinoserrano@gmail.com>
2021-07-26 12:01:49 -0300

    fix: fixed theme breakpoints

--- src/styles/theme/breakpoints/devices.ts
+++ src/styles/theme/breakpoints/devices.ts
@@ -1,6 +1,6 @@
 export const devices = {
-  sm: '@media only screen and (max-width: 30rem)',
-  md: '@media only screen and (max-width: 48em)',
-  lg: '@media only screen and (max-width: 62em)',
-  xl: '@media only screen and (max-width: 80em)',
+  sm: '@media only screen and (min-width: 30rem)',
+  md: '@media only screen and (min-width: 48rem)',
+  lg: '@media only screen and (min-width: 62rem)',
+  xl: '@media only screen and (min-width: 80rem)',
 }
