31fe6cbf00d5e204aed19e7ecec6079ad208bdbb
vitorserrano <vitorcirinoserrano@gmail.com>
2021-07-25 23:36:21 -0300

    feat: create main component

---
+++ src/components/Main/About/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from '../../Container'
+
+export const About = () => {
+  return (
+    <Container variant="light">
+      <h1>Hello World</h1>
+    </Container>
+  )
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import { Banner } from './Banner'
+import { About } from './About'
+
+export const Main = () => (
+  <main>
+    <Banner />
+    <About />
+  </main>
+)
