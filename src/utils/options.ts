225bd19ecba81bb9c7339fbb61883ad700c356cf
vitorserrano <vitorcirinoserrano@gmail.com>
2022-09-07 04:25:32 -0300

    feat: create options file

---
+++ src/utils/options.ts
@@ -0,0 +1,7 @@
+export const options = [
+  { id: '1', name: 'Inicio', href: '/' },
+  { id: '2', name: 'Sobre', href: '/about' },
+  { id: '3', name: 'Projetos', href: '/projects' },
+  // { id: '4', name: 'Skills', href: '/skills' },
+  // { id: '5', name: 'Experiências', href: '/experiences' },
+]
