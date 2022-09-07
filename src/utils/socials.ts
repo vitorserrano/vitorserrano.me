a31c0bf6339481fc7af4c0066025541693899861
vitorserrano <vitorcirinoserrano@gmail.com>
2022-09-07 04:25:10 -0300

    feat: change social file

---
+++ src/utils/socials.ts
@@ -0,0 +1,32 @@
+export const socials = [
+  {
+    id: '1',
+    name: 'github',
+    icon: 'GithubLogo',
+    href: 'https://github.com/vitorserrano',
+  },
+  {
+    id: '2',
+    name: 'linkedin',
+    icon: 'LinkedinLogo',
+    href: 'https://www.linkedin.com/in/vitor-serrano/',
+  },
+  {
+    id: '3',
+    name: 'telegram',
+    icon: 'TelegramLogo',
+    href: 'https://t.me/virtuzera',
+  },
+  {
+    id: '4',
+    name: 'instagram',
+    icon: 'InstagramLogo',
+    href: 'https://www.instagram.com/virtuzera/',
+  },
+  {
+    id: '5',
+    name: 'twitter',
+    icon: 'TwitterLogo',
+    href: 'https://twitter.com/virtuzera',
+  },
+]
